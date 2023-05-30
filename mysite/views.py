2234c4ceec358f73bcfabeb6d17bfd3ae23159fb
Richardalphonse <richardriche96@gmail.com>
2023-05-30 10:08:55 +0530

    The demo project has been added

---
+++ mysite/views.py
@@ -0,0 +1,10 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from employees.models import Employees
+def home(request):
+    employees=Employees.objects.all()
+    context={
+        'employees':employees,
+    }
+    return render(request,"home.html")
